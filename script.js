68b5ba8ed6967d1e6c887d756b4b1053d37ab28d
Vinicius Drumm <dev.viniciusdrumm@gmail.com>
2024-07-09 20:00:44 -0300

    editado pág home

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+//SlideShow//
+
+let count = 1;
+document.getElementById("radio1").checked = "true";
+
+setInterval(function() {
+    nextImage();
+}, 5000)
+
+function nextImage(){
+    count++;
+    if(count > 3){
+        count = 1;
+    }
+
+    document.getElementById("radio"+count).checked = "true";
+}
+
+//Fim SlideShow//
 
 // Função Carrinho de compras
 
